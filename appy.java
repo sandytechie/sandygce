e1b2d3791d5f4564f9242fc6497f5fcd00fee17f
santhiya1 <santhiyamurugan412@gmail.com>
2017-10-01 17:58:38 +1400

    first commit

---
+++ appy.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+import java.applet.*;
+public class appy extends Applet
+{
+
+public void paint(Graphics g)
+{
+g.drawString("welome",100,100);
+}
+}
+//<applet code=priya width=500 height=500></applet>
